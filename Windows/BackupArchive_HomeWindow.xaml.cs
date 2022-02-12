7ff9ad74c9e3d1853569306a92a4f2d2d709d53f
Mert Gunduz <merttgg@gmail.com>
2022-02-12 18:13:38 +0300

    Livecharts Added 📈 🐾

--- Windows/BackupArchive_HomeWindow.xaml.cs
+++ Windows/BackupArchive_HomeWindow.xaml.cs
@@ -9,6 +9,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
+​
 
 namespace BackupArchive.Windows
 {
